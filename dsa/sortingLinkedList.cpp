2bd196ff228743577e5f8f634d80161ac4fb9397
rexdy <jnani@duck.com>
2024-11-29 20:47:59 +0530

    small change

--- dsa/sortingLinkedList.cpp
+++ dsa/sortingLinkedList.cpp
@@ -1,46 +1,16 @@
+
 #include <iostream>
-
 using std::cin, std::cout;
-
 typedef struct node{
     int data;
     struct node* next;
 }node;
-
-node* insertToSortedHead(node* sortedHead, node* newNode){
-    if (!sortedHead || sortedHead->data >= newNode->data) {
-        newNode->next = sortedHead;
-        return newNode;
-    }
-    node* curr = sortedHead;
-    while (curr->next && curr->next->data < newNode->data) {
-        curr = curr->next;
-    }
-    newNode->next = curr->next;
-    curr->next = newNode;
-    return sortedHead;
-}
 
 node* createNewNode(int value){
     node* newNode = (node*)malloc(sizeof(node));
     newNode->data = value;
     newNode->next = NULL;
     return newNode;
-}
-
-void display(node* head){
-    if(!head){
-        cout << "empty\n";
-        return;
-    }
-    else{
-        while(head){
-            cout << head->data << " ";
-            head = head->next;
-        }
-        cout << "\n";
-        return;
-    }
 }
 
 node* insertAtEnd(node* head, int value){
@@ -56,6 +26,35 @@ node* insertAtEnd(node* head, int value){
         curr->next = createNewNode(value);
         return head;
     }
+}
+
+void display(node* head){
+    if(!head){
+        cout << "empty list\n";
+        return;
+    }
+    else{
+        while(head){
+            cout << head->data << " ";
+            head = head->next;
+        }
+        cout << "\n";
+        return;
+    }
+}
+
+node* insertToSortedHead(node* sortedHead, node* newNode){
+    if (!sortedHead || sortedHead->data >= newNode->data) {
+        newNode->next = sortedHead;
+        return newNode;
+    }
+    node* curr = sortedHead;
+    while (curr->next && curr->next->data < newNode->data) {
+        curr = curr->next;
+    }
+    newNode->next = curr->next;
+    curr->next = newNode;
+    return sortedHead;
 }
 
 node* sortList(node* unsortedHead, node* sortedHead){
@@ -79,18 +78,8 @@ int main(){
     node* unsortedHead = NULL;
     node* sortedHead = NULL;
 
-    /*unsortedHead = insertAtEnd(unsortedHead, 9);
-    unsortedHead = insertAtEnd(unsortedHead, 2);
-    unsortedHead = insertAtEnd(unsortedHead, 7);
-    unsortedHead = insertAtEnd(unsortedHead, 13);
-    unsortedHead = insertAtEnd(unsortedHead, 0);
-    unsortedHead = insertAtEnd(unsortedHead, 69);
-    display(unsortedHead);
-    sortedHead = sortList(unsortedHead, sortedHead);
-    display(sortedHead);*/
-
     int uin = 0;
-    while(uin!=5){
+    while(1){
         cout << "Choose an option:\n1.Insert to List\n2.Display\n3.Sort\n4.Show Sorted\n5.Exit\n";
         cin >> uin;
         switch (uin){
