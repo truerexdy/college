0afa34e34c50e4a22452118b3f75965d5456db93
rexdy <jnani@duck.com>
2024-11-29 18:58:04 +0530

    small changes

--- dsa/sortingLinkedList.cpp
+++ dsa/sortingLinkedList.cpp
@@ -1,124 +1,76 @@
 #include <iostream>
 
-using std::cin, std::cout;
-
-typedef struct node{
-    int data;
-    struct node* next;
-}node;
-
-node* insertToSortedHead(node* sortedHead, node* newNode){
-    if (!sortedHead || sortedHead->data >= newNode->data) {
-        newNode->next = sortedHead;
-        return newNode;
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode(int x){
+        this->val = x;
+        this->next = nullptr;
     }
-    node* curr = sortedHead;
-    while (curr->next && curr->next->data < newNode->data) {
-        curr = curr->next;
-    }
-    newNode->next = curr->next;
-    curr->next = newNode;
-    return sortedHead;
-}
+};
 
-node* createNewNode(int value){
-    node* newNode = (node*)malloc(sizeof(node));
-    newNode->data = value;
-    newNode->next = NULL;
-    return newNode;
-}
-
-void display(node* head){
-    if(!head){
-        cout << "empty\n";
-        return;
-    }
-    else{
-        while(head){
-            cout << head->data << " ";
-            head = head->next;
+class Solution {
+public:
+    ListNode* insertIntoSortedList(ListNode* sortedHead, ListNode* newNode) {
+        if (sortedHead == nullptr || sortedHead->val >= newNode->val) {
+            newNode->next = sortedHead;
+            return newNode;
         }
-        cout << "\n";
-        return;
-    }
-}
-
-node* insertAtEnd(node* head, int value){
-    if(!head){
-        head = createNewNode(value);
-        return head;
-    }
-    else{
-        node* curr = head;
-        while(curr->next){
-            curr = curr->next;
+        ListNode* current = sortedHead;
+        while (current->next != nullptr && current->next->val < newNode->val) {
+            current = current->next;
         }
-        curr->next = createNewNode(value);
-        return head;
+        newNode->next = current->next;
+        current->next = newNode;
+        return sortedHead;
     }
-}
-
-node* sortList(node* unsortedHead, node* sortedHead){
-    if(!unsortedHead){
-        cout << "Empty list";
-        return nullptr;
-    }
-    else{
-        node* curr = unsortedHead;
-        while(curr){
-            node* nextNode = curr->next;
-            curr->next = nullptr;
-            sortedHead = insertToSortedHead(sortedHead, curr);
-            curr = nextNode;
+    ListNode* insertionSortList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* sortedHead = head;
+        ListNode* unsortedHead = head->next;
+        sortedHead->next = nullptr;
+        while (unsortedHead != nullptr) {
+            ListNode* nextNode = unsortedHead->next;
+            unsortedHead->next = nullptr;
+            sortedHead = insertIntoSortedList(sortedHead, unsortedHead);
+            unsortedHead = nextNode;
         }
         return sortedHead;
     }
-}
-
-int main(){
-    node* unsortedHead = NULL;
-    node* sortedHead = NULL;
-
-    /*unsortedHead = insertAtEnd(unsortedHead, 9);
-    unsortedHead = insertAtEnd(unsortedHead, 2);
-    unsortedHead = insertAtEnd(unsortedHead, 7);
-    unsortedHead = insertAtEnd(unsortedHead, 13);
-    unsortedHead = insertAtEnd(unsortedHead, 0);
-    unsortedHead = insertAtEnd(unsortedHead, 69);
-    display(unsortedHead);
-    sortedHead = sortList(unsortedHead, sortedHead);
-    display(sortedHead);*/
-
-    int uin = 0;
-    while(uin!=5){
-        cout << "Choose an option:\n1.Insert to List\n2.Display\n3.Sort\n4.Show Sorted\n5.Exit\n";
-        cin >> uin;
-        switch (uin){
-        case 1:
-            cout << "Enter the new node value:\n";
-            int temp;
-            cin >> temp;
-            unsortedHead = insertAtEnd(unsortedHead, temp);
-            break;
-        case 2:
-            display(unsortedHead);
-            break;
-        case 3:
-            sortedHead = sortList(unsortedHead, sortedHead);
-            cout << "Unsorted\n";
-            display(unsortedHead);
-            cout << "Sorted\n";
-            display(sortedHead);
-            break;
-        case 4:
-            display(sortedHead);
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            break;
+    void printList(ListNode* head) {
+        while (head != nullptr) {
+            std::cout << head->val << " ";
+            head = head->next;
         }
+        std::cout << std::endl;
     }
+    ListNode* createList(int arr[], int n) {
+        if (n == 0) return nullptr;
+        
+        ListNode* head = new ListNode(arr[0]);
+        ListNode* current = head;
+        
+        for (int i = 1; i < n; i++) {
+            current->next = new ListNode(arr[i]);
+            current = current->next;
+        }
+        
+        return head;
+    }
+};
+
+int main() {
+    Solution solution;
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    ListNode* head = solution.createList(arr, n);
+    std::cout << "Original List: ";
+    solution.printList(head);
+    head = solution.insertionSortList(head);
+    std::cout << "Sorted List:   ";
+    solution.printList(head);
     return 0;
 }
